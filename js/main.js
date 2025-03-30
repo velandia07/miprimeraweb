fd744683560e656a2443b32bbd4365254f59c2fb
Miguel <miguelvelandia07@outlook.com.com>
2025-03-30 00:13:29 -0500

    Se agrega document.addEventListener para que mi script no se ejecute antes del DOM

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Esto hará que mi script no se ejecute antes del DOM
+document.addEventListener("DOMContentLoaded", function() {
 // Obtenemos los elementos del formulario HTML
 const formCalculo = document.getElementById('form-calculo'); // Formulario principal
 const saborInput = document.getElementById('sabor');          // Selector de sabor de torta
@@ -123,3 +125,4 @@ function validarFormulario() {
       window.open(url, '_blank');
     }
   }
+});
